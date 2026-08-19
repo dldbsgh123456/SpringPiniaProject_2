eb61bcd6aa5d6cf30d31f5494205e8d8ca5909d1
dldbsgh124584-commits <dldbsgh124584@gmail.com>
2026-08-19 16:12:35 +0900

    third

--- src/main/java/com/sist/web/config/SecurityConfig.java
+++ src/main/java/com/sist/web/config/SecurityConfig.java
@@ -121,27 +121,27 @@ public class SecurityConfig {
     *   
     */
    // 인증 관리자 
-   @Bean
-   public AuthenticationManager authenticationManager(
-       HttpSecurity http,
-       BCryptPasswordEncoder passwordEncoder
-   ) throws Exception
-   {
-	   return null;
-   }
-   @Bean
-   public JdbcUserDetailsManager jdbcUserDetailsSevice()
-   {
-	   return null;
-   }
-   // 비밀번호 암호화 
-   @Bean
-   public BCryptPasswordEncoder passwordEncoder() {
-	   return new BCryptPasswordEncoder();
-   }
-   // PersistentLogins 등록
-   @Bean
-   public PersistentTokenRepository persistentTokenRepository() {
-	   return null;
-   }
+//   @Bean
+//   public AuthenticationManager authenticationManager(
+//       HttpSecurity http,
+//       BCryptPasswordEncoder passwordEncoder
+//   ) throws Exception
+//   {
+//	   return null;
+//   }
+//   @Bean
+//   public JdbcUserDetailsManager jdbcUserDetailsSevice()
+//   {
+//	   return null;
+//   }
+//   // 비밀번호 암호화 
+//   @Bean
+//   public BCryptPasswordEncoder passwordEncoder() {
+//	   return new BCryptPasswordEncoder();
+//   }
+//   // PersistentLogins 등록
+//   @Bean
+//   public PersistentTokenRepository persistentTokenRepository() {
+//	   return null;
+//   }
 }
