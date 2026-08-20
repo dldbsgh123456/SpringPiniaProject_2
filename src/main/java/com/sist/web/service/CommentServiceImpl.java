166425b30b2e8554c4521b5fcfdf761a6a1a95ba
dldbsgh124584-commits <dldbsgh124584@gmail.com>
2026-08-20 15:01:45 +0900

    11111

---
+++ src/main/java/com/sist/web/service/CommentServiceImpl.java
@@ -0,0 +1,33 @@
+package com.sist.web.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.sist.web.mapper.CommentMapper;
+import com.sist.web.vo.CommentVO;
+
+import lombok.RequiredArgsConstructor;
+@Service
+@RequiredArgsConstructor
+public class CommentServiceImpl implements CommentService{
+        private final CommentMapper cmapper;
+	@Override
+	public List<CommentVO> commentListData(int start,int fno) {
+		// TODO Auto-generated method stub
+		return cmapper.commentListData(start,fno);
+	}
+
+	@Override
+	public int commentRowCount(int fno) {
+		// TODO Auto-generated method stub
+		return cmapper.commentRowCount(fno);
+	}
+
+	@Override
+	public void commentInsert(CommentVO vo) {
+		// TODO Auto-generated method stub
+		cmapper.commentInsert(vo);
+	}
+
+}
